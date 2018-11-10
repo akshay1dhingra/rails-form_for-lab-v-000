ac072199682fa32dd717be5f26cb2f1ffe1b588b
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-11-10 03:35:16 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,8 +1,26 @@
 class SchoolClassesController < ApplicationController
 
+  def new
+    @student = Student.new
+  end
+
   def create
     @school_class = SchoolClass.create(params.require(:school_class))
     redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    redirect_to student_path(@student)
   end
 
 end
