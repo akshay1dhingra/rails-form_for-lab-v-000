f78f6f7d4fb2725429365fa42ed52e49a169c076
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-11-10 03:31:15 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,4 +9,14 @@ class StudentsController < ApplicationController
     redirect_to student_path(@student)
   end
 
+  def show 
+    @student = Student.find(params[:id])
+  end 
+
+  def update 
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    redirect_to student_path(@student)
+  end 
+
 end
